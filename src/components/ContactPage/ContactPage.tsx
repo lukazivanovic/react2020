7f1ecb3f9aebcd2f51253c64f3683322b11f1b4e
lukazivanovic <sdluka@hotmail.com>
2020-08-24 19:02:51 +0200

    Logout mechanism

--- src/components/ContactPage/ContactPage.tsx
+++ src/components/ContactPage/ContactPage.tsx
@@ -2,18 +2,21 @@ import React from 'react';
 import { Container, Card } from 'react-bootstrap';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faPhone } from '@fortawesome/free-solid-svg-icons';
+import RoledMainMenu from '../RoledMainMenu/RoledMainMenu';
 
 export default class ContactPage extends React.Component {
     render() {
         return (
             <Container>
+                <RoledMainMenu role="user" />
+
                 <Card>
                     <Card.Body>
                         <Card.Title>
                             <FontAwesomeIcon icon={ faPhone } /> Contact details
                         </Card.Title>
                         <Card.Text>
-                            Contact details will be here...
+                            Contact details will be show here...
                         </Card.Text>
                     </Card.Body>
                 </Card>
